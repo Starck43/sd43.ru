949a0b1f269f09fbd8746a9c4e0575aad050eb39
Stanislav Shabalin <saloon.as@gmail.com>
2026-01-19 23:43:10 +0100

    indent issue fixed

--- exhibition/forms.py
+++ exhibition/forms.py
@@ -69,22 +69,23 @@ class AccountSignupForm(SignupForm):
 
 class MultipleFileInput(forms.ClearableFileInput):
 	"""Кастомный виджет для множественной загрузки файлов"""
-    allow_multiple_selected = True
+	allow_multiple_selected = True
 
 
 class MultipleFileField(forms.FileField):
 	"""Кастомное поле для множественной загрузки файлов"""
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault("widget", MultipleFileInput())
-        super().__init__(*args, **kwargs)
+	def __init__(self, *args, **kwargs):
+		kwargs.setdefault("widget", MultipleFileInput())
+		super().__init__(*args, **kwargs)
 
-    def clean(self, data, initial=None):
-        single_file_clean = super().clean
-        if isinstance(data, (list, tuple)):
-            result = [single_file_clean(d, initial) for d in data]
-        else:
-            result = [single_file_clean(data, initial)]
-        return result
+	def clean(self, data, initial=None):
+		single_file_clean = super().clean
+		if isinstance(data, (list, tuple)):
+			result = [single_file_clean(d, initial) for d in data]
+		else:
+			result = [single_file_clean(data, initial)]
+		
+		return result
 
 
 class CustomClearableFileInput(ClearableFileInput):
